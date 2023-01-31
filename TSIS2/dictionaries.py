5d0229e687b4938d5c3f126635fad2a7ca5fbaad
Jalil Ramazan <j_ramazan@kbtu.kz>
2023-02-01 00:36:02 +0600

    Create dictionaries.py

---
+++ TSIS2/dictionaries.py
@@ -0,0 +1,4 @@
+a = {'Student1' : "22B03450", 'Student2' : '22B01245'}
+print(a.keys())
+print(a.values())
+#for example we have the names of students and their id, in this case the name will be key of some value(ID)
